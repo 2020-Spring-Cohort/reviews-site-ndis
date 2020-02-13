450e182310dc74a5de76f999e3c85a1027eed782
Daniel Villasmil <dvillasmil08@gmail.com>
2020-02-13 13:56:24 -0500

    Refactor

---
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -0,0 +1,34 @@
+package org.wecancodeit.reviews;
+
+public class Review {
+    String laptopName;
+    String laptopBrand;
+    String reviewText;
+    String hashTag;
+
+
+    public Review(String laptopName, String laptopBrand, String hashTag, String reviewText) {
+        this.laptopName = laptopName;
+        this.laptopBrand = laptopBrand;
+        this.hashTag = hashTag;
+        this.reviewText = reviewText;
+    }
+
+    public String getLaptopName() {
+        return laptopName;
+    }
+
+    public String getLaptopBrand() {
+        return laptopBrand;
+
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public String getHashTag() {
+        return hashTag;
+    }
+
+}
